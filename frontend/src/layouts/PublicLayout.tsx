e0ac40e484dacfe04e02f0ca6b382dfc31d4f9dd
N Kesava Krishna <kesavakrishna1006@gmail.com>
2026-04-02 11:39:50 +0530

    Added layouts folder

---
+++ frontend/src/layouts/PublicLayout.tsx
@@ -0,0 +1,14 @@
+import "../css/PublicLayout.css"
+import Navbar from "../components/Navbar"
+import { Outlet } from "react-router-dom"
+
+export default function PublicLayout() {
+  return (
+    <>
+      <Navbar />
+      <div className="content">
+        <Outlet />
+      </div>
+    </>
+  )
+}
