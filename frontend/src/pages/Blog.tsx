17b65d5bbe2dd720cf1b53fb10ae12d1ba9d2121
N Kesava Krishna <kesavakrishna1006@gmail.com>
2026-04-06 12:36:11 +0530

    added search bar

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -5,18 +5,34 @@ import api from '../api/axios'
 
 function Blog() {
   const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-   api.get("/posts/published")
+    api.get("/posts/published")
       .then(res => setPosts(res.data))
       .catch(err => console.log(err));
   }, []);
 
   return (
-    <div className="blog-container">
-      {posts.map((p: any) => (
-        <BlogCard key={p.id} post={p} />
-      ))}
+    <div>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search blogs..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
+      <div className="blog-container">
+        {posts
+          .filter((p: any) =>
+            p.title.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((p: any) => (
+            <BlogCard key={p.id} post={p} />
+          ))}
+      </div>
     </div>
   );
 }
